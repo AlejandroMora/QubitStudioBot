79b29a3d7b194a8c9d0ce055b7105417dff028c2
Alejandro Mora <quesomora@gmail.com>
2017-01-13 15:12:13 -0600

    SetUp

--- index.php
+++ index.php
@@ -1,27 +1,43 @@
-<html>
-<head></head>
-<body>
-    <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-            appId      : '1124745574267687',
-            xfbml      : true,
-            version    : 'v2.8'
-            });
-            FB.AppEvents.logPageView();
-        };
+<?php 
+    $challenge = $_REQUEST['hub_challenge'];
+    $verify_token = $_REQUEST['hub_verify_token'];
 
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-    
-    <?php 
-        print_r($_GET["hub_challenge"]);
-    ?>
-</body>
-</html>
+    if ($verify_token === 'EAAPZB8zxquycBAN6dZCLjGdnsZBgj3pHZC49KiyOkZA6jlX9PJYqBKSZBCCAVg2PhwWBjCl78zsUwBOpzamFIRHBrWrg7HQy0ufYE1g5n5k1yuUE5jZAZC4Kv2INkNMNeLi3UAyi62veiaOsr5BDCLEUBEqwWOXTNsICZCxHCMZBCz8AZDZD') {
+        echo $challenge;
+    }
+    //Token of app
+    $row = "EAAPZB8zxquycBAN6dZCLjGdnsZBgj3pHZC49KiyOkZA6jlX9PJYqBKSZBCCAVg2PhwWBjCl78zsUwBOpzamFIRHBrWrg7HQy0ufYE1g5n5k1yuUE5jZAZC4Kv2INkNMNeLi3UAyi62veiaOsr5BDCLEUBEqwWOXTNsICZCxHCMZBCz8AZDZD";
+
+
+    $input = json_decode(file_get_contents('php://input'), true);
+
+    $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+    $message = $input['entry'][0]['messaging'][0]['message']['text'];
+
+    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$row;
+
+    $ch = curl_init($url);
+
+    //Answer to the message adds 1
+    if($message)
+    {
+    $jsonData = '{
+        "recipient":{
+            "id":"'.$sender.'"
+        }, 
+        "message":{
+            "text":"'.$message. ' 1' .'"
+        }
+    }';
+    };
+
+    $json_enc = $jsonData;
+
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_enc);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
+
+    if(!empty($input['entry'][0]['messaging'][0]['message'])){
+        $result = curl_exec($ch);
+    }
+?>
