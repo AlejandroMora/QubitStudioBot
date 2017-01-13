e70bd7a411ef9e9d94dddae59059f06c6976f8e0
Alejandro Mora <quesomora@gmail.com>
2017-01-13 17:51:46 -0600

    SetUp

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php 
-    $challenge = $_REQUEST['hub_challenge'];
-    $verify_token = $_REQUEST['hub_verify_token'];
-    
-    //if ($verify_token === 'TOKENAPP') {
-        echo $challenge;
-    /*}
-    $row = "TOKENAPP";
+    $configJSON = json_decode(file_get_contents('./config.json'));
+    $token = $configJSON['appAccessToken'];
+    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$token;
+
     $input = json_decode(file_get_contents('php://input'), true);
     $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
-    $message = $input['entry'][0]['messaging'][0]['message']['text'];
+    $message = $input['entry'][0]['messaging'][0]['message']['text']; 
 
-    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$row;*/
+    $response = array(
+        'recipient' => array('id' => "$sender"),
+        'message' => array('text' => "I am an automated bot.")
+    );
+    $headers = array(
+        'http' => array(
+            'method' => 'POST',
+            'content' => json_encode($response),
+            'header' => "Content-Type: application/json\n"
+        )
+    );
+
+    $context = stream_context_create($headers);
+    file_get_contents($url, false, $context);
 ?>
