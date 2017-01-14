e0dca91903c80e245e8d3d8c281089af5b4f00c6
Alejandro Mora <quesomora@gmail.com>
2017-01-13 22:07:38 -0600

    OK OK

--- index.php
+++ index.php
@@ -8,16 +8,21 @@
     $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
     $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
-    $answer = ':)';
-    $response = array(
-        'recipient' => array('id' => $sender),
-        'message' => array('text' => $answer)
-    );
 
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-	curl_exec($ch);
-	curl_close($ch);
+    if(!empty($message)) {
+        $answer = " world :)";
+        if($message != "hello") {
+            $answer = "Try again";
+        }
+        $response = array(
+            'recipient' => array('id' => $sender),
+            'message' => array('text' => $answer)
+        );
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_exec($ch);
+        curl_close($ch);
+    }
 ?>
