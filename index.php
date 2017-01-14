de6ab535094fc117028e7b167ee7753273dcb60a
Alejandro Mora <quesomora@gmail.com>
2017-01-13 20:01:46 -0600

    Stop

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-
-    file_put_contents("fb.txt", file_get_contents("php://input"));
-
     $configJSON = json_decode(file_get_contents('./config.json'),true);
     $token = $configJSON['appAccessToken'];
     $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$token;
@@ -10,7 +7,7 @@
     //print_r($_REQUEST['hub_challenge']);
     $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
     $message = $input['entry'][0]['messaging'][0]['message']['text'];
-
+    /*
     $response = array(
         'recipient' => array('id' => "$sender"),
         'message' => array('text' => "Hi")
@@ -22,8 +19,7 @@
             'header' => "Content-Type: application/json\n"
         )
     );
-
     $context = stream_context_create($headers);
     file_get_contents("$url", false, $context);
-    
+    */
 ?>
