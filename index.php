591ce1a96e844036aacf948e474408aea37b403d
Alejandro Mora <quesomora@gmail.com>
2017-01-13 21:59:13 -0600

    OK :)

--- index.php
+++ index.php
@@ -7,19 +7,17 @@
     //print_r($_REQUEST['hub_challenge']);
     $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
     $message = $input['entry'][0]['messaging'][0]['message']['text'];
-    
+
+    $answer = ':)';
     $response = array(
-        'recipient' => array('id' => "$sender"),
-        'message' => array('text' => ":)")
+        'recipient' => array('id' => $sender),
+        'message' => array('text' => $answer)
     );
-    $headers = array(
-        'http' => array(
-            'method' => 'POST',
-            'content' => json_encode($response),
-            'header' => "Content-Type: application/json\n"
-        )
-    );
-    /*$context = stream_context_create($headers);
-    file_get_contents("$url", false, $context);
-    */
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+	curl_exec($ch);
+	curl_close($ch);
 ?>
