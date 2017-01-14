df2b5b27f8035b2e96dbb2fa9394961ff189602e
Alejandro Mora <quesomora@gmail.com>
2017-01-13 19:47:45 -0600

    SetUp - Ok

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$token;
 
     $input = json_decode(file_get_contents('php://input'), true);
-    print_r($_REQUEST['hub_challenge']);
-    $sender = $input->entry[0]->messaging[0]->sender->id;
-    $message = $input->entry[0]->messaging[0]->message->text; 
+    //print_r($_REQUEST['hub_challenge']);
+    $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+    $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
     $response = array(
         'recipient' => array('id' => "$sender"),
-        'message' => array('text' => "I am an automated bot.")
+        'message' => array('text' => "Hi")
     );
     $headers = array(
         'http' => array(
