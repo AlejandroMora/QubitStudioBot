eb995470c30edc6954d66d5eade8cca39d733f9a
Alejandro Mora <quesomora@gmail.com>
2017-01-13 23:19:17 -0600

    eoPHP

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     }
 
     $configJSON = json_decode(file_get_contents('./config.json'),true);
-    $token = $configJSON['appAccessToken'];
+    $token = $configJSON['pageAccessToken'];
     $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$token;
 
     $input = json_decode(file_get_contents('php://input'), true);
@@ -18,10 +18,23 @@
             'recipient' => array('id' => "$sender"),
             'message' => array('text' => "$answer")
         );
+
+        $headers = array(
+            'http' => array(
+                'method' => 'POST',
+                'content' => json_encode($response),
+                'header' => "Content-Type: application/json\n"
+            )
+        );
+        $context = stream_context_create($headers);
+        file_get_contents("$url", false, $context);
+        http_response_code(200);
+        /*
         $ch = curl_init("$url");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_exec($ch);
+        */
     }
 ?>
